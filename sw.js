40ae4e14c8fdf0b870acd2e8e622827e4f40d1c3
19890825h-cell <19890825h-cell@users.noreply.github.com>
2026-06-01 19:26:26 +0900

    Clarify background push privacy and refresh service worker

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
-const CACHE_NAME = "pressure-care-v12";
+const CACHE_NAME = "pressure-care-v13";
 const APP_FILES = ["./", "./index.html", "./styles.css", "./app.js", "./push-config.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches
       .keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))),
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
   );
 });
 
